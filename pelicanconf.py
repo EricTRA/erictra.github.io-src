7be1a0cb74d2ac36c2d580eb36e477a19e2824c7
Eric Van Steenbergen <eric.vansteenbergen@esl-asia.com>
2015-10-16 08:05:48 +0800

    Update source with links

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,15 +22,15 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('A world without Linux', 'http://www.linuxfoundation.org/world-without-linux'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn profile', 'https://www.linkedin.com/in/ericvansteenbergen'),
+          ('GitHub Page', 'http://erictra.github.io'),)
 
 DEFAULT_PAGINATION = 10
 
 THEME = "/usr/lib/python2.7/site-packages/pelican/themes/bootstrap2-dark"
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
